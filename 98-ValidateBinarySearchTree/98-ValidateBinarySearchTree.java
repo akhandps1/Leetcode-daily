9352665b15e3f5efe23665f659e99feadf8c8cbf
Akhand Pratap Singh <akhandps2007@gmail.com>
2026-07-20 12:23:41 -0700

    Create 98-ValidateBinarySearchTree

---
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.java
@@ -0,0 +1,35 @@
+// Last updated: 7/21/2026, 12:53:40 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        List<Integer>list=new ArrayList<>();
+        inorder(root,list);
+        for(int i=0;i<list.size()-1;i++){
+            if(list.get(i)>=list.get(i+1))
+            return false;
+        }
+        return true;
+        
+    }
+    private void inorder(TreeNode node,List<Integer>list){
+        if(node==null)
+        return;
+        inorder(node.left,list);
+        list.add(node.val);
+        inorder(node.right,list);
+    }
+}
