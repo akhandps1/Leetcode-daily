2f9f5ac6295d3a67d8cc643f343fe9f469740720
Akhand Pratap Singh <akhandps2007@gmail.com>
2026-07-20 12:21:36 -0700

    Create 1584-AverageSalaryExcludingTheMinimumAndMaximumSalary

---
+++ 1584-AverageSalaryExcludingTheMinimumAndMaximumSalary/1584-AverageSalaryExcludingTheMinimumAndMaximumSalary.java
@@ -0,0 +1,16 @@
+// Last updated: 7/21/2026, 12:51:36 AM
+class Solution {
+    public double average(int[] salary) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        double sum = 0;
+
+        for (int x : salary) {
+            min = Math.min(min, x);
+            max = Math.max(max, x);
+            sum += x;
+        }
+        return (sum - min - max) / (salary.length - 2);
+        
+    }
+}
